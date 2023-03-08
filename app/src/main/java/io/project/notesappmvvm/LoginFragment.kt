d3ec6f66f020381e432ddae49739fb624640bb06
vansh-tandon <93190207+vansh-tandon@users.noreply.github.com>
2023-03-08 23:51:05 +0530

    Navigation updated

--- app/src/main/java/io/project/notesappmvvm/LoginFragment.kt
+++ app/src/main/java/io/project/notesappmvvm/LoginFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import io.project.notesappmvvm.databinding.FragmentLoginBinding
+import io.project.notesappmvvm.databinding.FragmentRegisterBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //setting binding obj to null, to free up memory
+        _binding = null
+    }
 }
--- app/src/main/java/io/project/notesappmvvm/RegisterFragment.kt
+++ app/src/main/java/io/project/notesappmvvm/RegisterFragment.kt
@@ -7,24 +7,39 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import io.project.notesappmvvm.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
 
+    private var _binding:FragmentRegisterBinding? = null
+    //to make it null safe
+    //binding is pointing to
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
         //and the corresponding view will be stored in this variable
-        val view = inflater.inflate(R.layout.fragment_register, container, false)
-        val txtRedirect = view.findViewById<TextView>(R.id.txtRedirect)
-        txtRedirect.setOnClickListener {
+//        val view = inflater.inflate(R.layout.fragment_register, container, false)
+//        val txtRedirect = view.findViewById<TextView>(R.id.txtRedirect)
+        binding.btnLogin.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
 
-        return view
+//        return view
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        //setting binding obj to null, to free up memory
+        _binding = null
+    }
 
 }
